a21c25a2ff6bc7494cbaf89a5055dafdca6713e7
Faizan7012 <frtoons7012@gmail.com>
2023-03-24 11:11:14 +0530

    new

--- src/Route/sprint.route.js
+++ src/Route/sprint.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { getSprint, delSprint, createSprint } = require("../Controller/sprint.controller");
+const taskModel = require("../Model/task.model");
 
 
 const sprintRoute = express.Router();
@@ -8,4 +9,20 @@ const sprintRoute = express.Router();
 sprintRoute.get("/", getSprint )
 sprintRoute.post("/", createSprint )
 sprintRoute.delete("/:id", delSprint )
+sprintRoute.get("/tasks", async(req , res)=>{
+    try {
+        
+     let ans = await taskModel.aggregate([{$group : {_id : '$sprint' ,count: { $count: { }}} }]);
+     res.send({
+        status:true , 
+        data : ans
+     })
+
+    } catch (e) {
+        res.status(401).send({
+            status:false,
+            message : e.message
+        })
+    }
+} )
 module.exports = { sprintRoute };
